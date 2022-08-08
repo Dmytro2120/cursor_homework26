15218dff666bb035730262149b1d36aee5fdc873
Dmytro <godovdmytrro@gmail.com>
2022-08-08 17:38:13 +0200

    homework

---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+export default class Welcome extends React.Component {
+    render() {
+        return(
+            <div>
+            <h1 className="title"> Дякуємо за вхід </h1>
+        </div>
+         )
+    }
+        
+}
+
